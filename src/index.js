e596b3767ba00a86efb548cf89d3e0a41d0fa9d5
Safonova Alexandra <alexa-09@bk.ru>
2022-02-19 23:17:07 +0300

    test pass

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (arguments.length == 0) {
+    return [];
+  } else if (matrix.length == 0) {
+    return [];
+  }
+  else if (matrix.length < 4) {
+    matrix[1].sort((a, b) => b - a);
+  let newArr = matrix.flat();
+  return newArr;
+  }
+  else {
+    matrix[1].sort((a, b) => b - a);
+    matrix[3].sort((a, b) => b - a);
+  let newArr = matrix.flat();
+  return newArr;
+  }
+  
 }
